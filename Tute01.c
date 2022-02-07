92e266894aa058ad918ca2329beb1b39af23b0f4
Keegal-S-D <99182714+Keegal-S-D@users.noreply.github.com>
2022-02-08 02:14:21 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
+   int=mark1,mark2,sum;
+   float=avg;
+   printf("Enter Mark1");
+   scanf("%d",&mark1);
+   printf("Enter Mark2");
+   scanf("%d",&mark2);
   
+   sum=mark1+mark2;
+   avg=sum/2;
+   printf("Total of marks is %d/n",sum);
+   printf("Average of the marks is %f",avg);
   return 0;
 }
 
